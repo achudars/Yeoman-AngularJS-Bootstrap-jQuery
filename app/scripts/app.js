29dc549537b42c0a8cf93d5e6bd9a28472f9558f
Aleks <achudars@gmail.com>
2013-07-09 12:22:02 +0100

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var testApp = angular.module('testApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+testApp.controller('MainCtrl', ['$scope', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+}]);
